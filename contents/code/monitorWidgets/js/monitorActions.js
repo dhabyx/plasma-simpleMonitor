3c985fa6d8e643f8c30dfb915b98aea157693d5e
Dhaby Xiloj <dhabyx@gmail.com>
2014-05-28 13:29:42 -0600

    :memo: Adding GPL license and copyright, fix #5

--- contents/code/monitorWidgets/js/monitorActions.js
+++ contents/code/monitorWidgets/js/monitorActions.js
@@ -1,3 +1,22 @@
+/**
+ * Copyright 2013-2014 Dhaby Xiloj
+ *
+ * This file is part of plasma-simpleMonitor.
+ *
+ * plasma-simpleMonitor is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or any later version.
+ *
+ * plasma-simpleMonitor is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with plasma-simpleMonitor.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
 function modelAddData(model,index,value) {
     if (model.count <= index) {
         model.append(value)
